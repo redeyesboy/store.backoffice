2845682e84ec379ed4e354d8c35285e76b5f5aed
worker <redeyesboy@naver.com>
2024-08-23 15:46:28 +0900

    Runner 파일명 변경

--- backoffice/src/test/java/co/kr/store/bo/ApplicationTests.java
+++ backoffice/src/test/java/co/kr/store/bo/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackofficeApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
