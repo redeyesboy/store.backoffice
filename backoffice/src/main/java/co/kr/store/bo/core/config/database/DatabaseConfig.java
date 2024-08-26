26c0ca5c17ca528eeafef79563366f218e8fd421
worker <redeyesboy@naver.com>
2024-08-26 11:43:22 +0900

    Database Connection 설정 추가

---
+++ backoffice/src/main/java/co/kr/store/bo/core/config/database/DatabaseConfig.java
@@ -0,0 +1,36 @@
+package co.kr.store.bo.core.config.database;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+	@Primary @Bean
+	@ConfigurationProperties(prefix = "spring.datasource.hikari")
+	HikariConfig hikariConfig() {
+		return new HikariConfig();
+	}
+
+	@Primary @Bean
+	DataSource dataSource(HikariConfig hikariConfig) {
+		return new HikariDataSource(hikariConfig);
+	}
+
+	@Primary @Bean
+	PlatformTransactionManager platformTransactionManager(DataSource dataSource) {
+		DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager(dataSource);
+		dataSourceTransactionManager.setGlobalRollbackOnParticipationFailure(false);
+		return dataSourceTransactionManager;
+	}
+
+}
