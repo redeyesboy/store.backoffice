3f258fb3847203759a4a109f5e87ec4593938633
worker <redeyesboy@naver.com>
2024-09-13 10:37:11 +0900

    - 불필요자원정리 - 공통 utils method 추가

--- backoffice/src/main/java/co/kr/store/bo/common/global/GlobalController.java
+++ backoffice/src/main/java/co/kr/store/bo/common/global/GlobalController.java
@@ -1,16 +1,18 @@
 package co.kr.store.bo.common.global;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class GlobalController {
 
-	@GetMapping({ "/" })
+	@RequestMapping({ "/" })
 	public ModelAndView mainView() {
 		return new ModelAndView("/view/main/mainView");
 	}
--- backoffice/src/main/java/co/kr/store/bo/common/utils/BeanUtils.java
+++ backoffice/src/main/java/co/kr/store/bo/common/utils/BeanUtils.java
@@ -4,6 +4,13 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Component
 public class BeanUtils implements ApplicationContextAware {
@@ -21,6 +28,30 @@ public class BeanUtils implements ApplicationContextAware {
 
 	public static <T> T getBean(String name, Class<T> requiredType) {
 		return applicationContext.getBean(name, requiredType);
+	}
+
+	public static ServletContext getServletContext() {
+		return BeanUtils.getHttpServletRequest().getServletContext();
+	}
+
+	public static HttpServletRequest getHttpServletRequest() {
+		return ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+	}
+
+	public static HttpServletResponse getHttpServletResponse() {
+		return ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getResponse();
+	}
+
+	public static HttpSession getHttpSession() {
+		return BeanUtils.getHttpServletRequest().getSession();
+	}
+
+	public static String getRequestHeader(String name) {
+		return BeanUtils.getHttpServletRequest().getHeader(name);
+	}
+
+	public static String getResponseHeader(String name) {
+		return BeanUtils.getHttpServletResponse().getHeader(name);
 	}
 
 }
