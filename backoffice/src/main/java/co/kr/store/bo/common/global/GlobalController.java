f1262b12015512b91b07738a32ce3672c3b6335c
worker <redeyesboy@naver.com>
2024-09-13 09:45:48 +0900

    컨트롤러 변경

--- backoffice/src/main/java/co/kr/store/bo/common/global/GlobalController.java
+++ backoffice/src/main/java/co/kr/store/bo/common/global/GlobalController.java
@@ -1,22 +1,18 @@
-package co.kr.store.bo.main.controller;
+package co.kr.store.bo.common.global;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import co.kr.store.bo.main.service.MainService;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-public class MainController {
-
-	private final MainService mainService;
+public class GlobalController {
 
 	@GetMapping({ "/" })
 	public ModelAndView mainView() {
 		return new ModelAndView("/view/main/mainView");
 	}
-
 
 }
