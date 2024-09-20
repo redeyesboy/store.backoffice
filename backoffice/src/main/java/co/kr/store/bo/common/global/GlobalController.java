3a1c84049a7df38342756b6f8ba7253e838287ef
worker <redeyesboy@naver.com>
2024-09-20 10:39:01 +0900

    불필요 주석 삭제

--- backoffice/src/main/java/co/kr/store/bo/common/global/GlobalController.java
+++ backoffice/src/main/java/co/kr/store/bo/common/global/GlobalController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import co.kr.store.bo.common.utils.CodeUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,12 +14,6 @@ public class GlobalController {
 
 	@RequestMapping({ "/" })
 	public ModelAndView mainView() {
-
-		log.info("XXXXXXXXXX11111111111 :: {}" ,CodeUtils.getCommonCodeList("T_0001"));
-		log.info("XXXXXXXXXX22222222222 :: {}" ,CodeUtils.getCommonCodeList("T_0001"));
-		log.info("XXXXXXXXXX33333333333 :: {}" ,CodeUtils.getAllCommonCodeList("T_0001"));
-		log.info("XXXXXXXXXX44444444444 :: {}" ,CodeUtils.getAllCommonCodeList("T_0001"));
-
 		return new ModelAndView("/view/main/mainView");
 	}
 
